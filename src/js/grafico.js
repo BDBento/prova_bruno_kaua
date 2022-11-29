a0999bed41f1e9868689366a494c7f12f775bd3f
bruno bento <66705552+BDBento@users.noreply.github.com>
2022-11-28 22:10:42 -0400

    a
    a

--- src/js/grafico.js
+++ src/js/grafico.js
@@ -11,32 +11,20 @@ let options = {
 
 function graf() {
     // bom
-    let num1 = document.querySelector('.bom1').textContent;
-    let bom1 = parseFloat(num1);
-    let num2 = document.querySelector('.bom2').textContent;
-    let bom2 = parseFloat(num2);
-    let num3 = document.querySelector('.bom3').textContent;
-    let bom3 = parseFloat(num3);
-    let num4 = document.querySelector('.bom4').textContent;
-    let bom4 = parseFloat(num4);
+    let bom1 = parseFloat(document.querySelector('.bom1').textContent);
+    let bom2 = parseFloat(document.querySelector('.bom2').textContent);
+    let bom3 = parseFloat(document.querySelector('.bom3').textContent);
+    let bom4 = parseFloat(document.querySelector('.bom4').textContent);
     // medio
-    let num5 = document.querySelector('.medio1').textContent;
-    let medio1 = parseFloat(num5);
-    let num6 = document.querySelector('.medio2').textContent;
-    let medio2 = parseFloat(num6);
-    let num7 = document.querySelector('.medio3').textContent;
-    let medio3 = parseFloat(num7);
-    let num8 = document.querySelector('.medio4').textContent;
-    let medio4 = parseFloat(num8);
+    let medio1 = parseFloat(document.querySelector('.medio1').textContent);
+    let medio2 = parseFloat(document.querySelector('.medio2').textContent);
+    let medio3 = parseFloat(document.querySelector('.medio3').textContent);
+    let medio4 = parseFloat(document.querySelector('.medio4').textContent);
     // ruim
-    let num9 = document.querySelector('.ruim1').textContent;
-    let ruim1 = parseFloat(num9);
-    let num10 = document.querySelector('.ruim2').textContent;
-    let ruim2 = parseFloat(num10);
-    let num11 = document.querySelector('.ruim3').textContent;
-    let ruim3 = parseFloat(num11);
-    let num12 = document.querySelector('.ruim4').textContent;
-    let ruim4 = parseFloat(num12);
+    let ruim1 = parseFloat(document.querySelector('.ruim1').textContent);
+    let ruim2 = parseFloat(document.querySelector('.ruim2').textContent);
+    let ruim3 = parseFloat(document.querySelector('.ruim3').textContent);
+    let ruim4 = parseFloat(document.querySelector('.ruim4').textContent);
 
     var data = google.visualization.arrayToDataTable([
         ['Year',        'Bom',     'Medio',    'ruim'],
